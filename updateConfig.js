0605f0ff35e55ba086c1d9b1a3ec595de1dc5e51
Augustus D'Souza <augiwan@users.noreply.github.com>
2020-11-11 20:54:33 +0530

    Update updateConfig.js

--- updateConfig.js
+++ updateConfig.js
@@ -4,6 +4,7 @@ const contents = fs.readFileSync("config.production.json");
 const config = JSON.parse(contents);
 const externalURL = process.env.url || process.env.RENDER_EXTERNAL_URL;
 config.mail.transport = "SMTP";
+config.mail.options = {};
 config.mail.options.service = "Mailgun";
 config.mail.options.auth.user = process.env.mail__options__auth__user;
 config.mail.options.auth.pass = process.env.mail__options__auth__pass;
